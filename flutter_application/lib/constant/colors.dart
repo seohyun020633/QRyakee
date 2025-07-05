cc60ee2cd87669568628f7b4f9544f4a01013c93
shAhn <shahn@shAhnui-MacBookAir.local>
2025-07-05 20:34:32 +0900

    color 추가& 마이페이지 UI 수정& 앱 이름 설정(plist)

--- flutter_application/lib/constant/colors.dart
+++ flutter_application/lib/constant/colors.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF00B97E);
-  static const Color blueGray = Colors.blueGrey;
+  static const Color sub = Color.fromARGB(255, 152, 186, 166);
+  static const Color light = Color.fromARGB(255, 209, 246, 225);
 }
