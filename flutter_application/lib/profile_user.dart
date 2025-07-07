6441df84ea369fb7f4b5247bbba22fd0a5ab8e4f
shAhn <shahn@shAhnui-MacBookAir.local>
2025-07-07 11:30:16 +0900

    오류수정2

--- flutter_application/lib/profile_user.dart
+++ flutter_application/lib/profile_user.dart
@@ -157,7 +157,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   (item) => buildInfoTile(
                     item['label'] as String,
                     item['value'] as String,
-                    item['onTap'] as VoidCallback,
+                    item['onTap'] as VoidCallback? ?? () {},
                   ),
                 ),
               ],
