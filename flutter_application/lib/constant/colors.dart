4b599db8f79634d2621e59ef2d5f3bfd28caccce
shAhn <shahn@shAhnui-MacBookAir.local>
2025-07-07 11:29:55 +0900

    오류 수정2

--- flutter_application/lib/constant/colors.dart
+++ flutter_application/lib/constant/colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color(0xFF00B97E);
   static const Color sub = Color.fromARGB(255, 152, 186, 166);
-  static const Color light = Color.fromARGB(255, 209, 246, 225);
+  static const Color lightgrey = Color.fromARGB(208, 232, 232, 232);
 }
--- flutter_application/lib/profile_user.dart
+++ flutter_application/lib/profile_user.dart
@@ -168,7 +168,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
           Container(
             width: double.infinity,
             height: 10,
-            color: AppColors.lighgrey,
+            color: AppColors.lightgrey,
           ),
 
           // ✅ 자주 간 약국 타이틀
